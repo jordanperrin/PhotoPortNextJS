7bcbe0f77b42abb8f363fd3efe5d5f4f168ef053
Jordan Perrin <jordanperrin@Jordans-iMac.local>
2024-01-10 12:27:23 -0500

    wip fixing svg/css across browsers

--- photo-portfolio/src/components/Footer/index.js
+++ photo-portfolio/src/components/Footer/index.js
@@ -50,7 +50,7 @@ export default function Footer(props){
           <svg className={styles.svg}>
             <filter id="wavy">
               <feTurbulence x="0" y="0" baseFrequency="0.109" numOctaves="5" seed="2">
-                <animate attributeName="baseFrequency" dur="60s" values="0.02;0.005;0.02" repeatCount="indefinite" />
+                <animate attributeName="baseFrequency" values="0.02;0.005;0.02" repeatCount="indefinite" />
               </feTurbulence>
               <feDisplacementMap in="SourceGraphic" scale="25" />
             </filter>
--- photo-portfolio/src/components/SvgNav/index.js
+++ photo-portfolio/src/components/SvgNav/index.js
@@ -4,10 +4,10 @@ export default function SvgNav(props){
     
     return(
         <>
-         <svg className={styles.svg}>
+         <svg id="burn" className={styles.svg}>
                         <filter id="wavy-nav">
                         <feTurbulence x="0" y="0" baseFrequency="0.109" numOctaves="4" seed="2">
-                            <animate attributeName="baseFrequency" dur="80s" values="0.02;0.005;0.02" repeatCount="indefinite" />
+                            <animate attributeName="baseFrequency" values="0.02;0.005;0.02"  />
                         </feTurbulence>
                         <feDisplacementMap in="SourceGraphic" scale="25" />
                         </filter>
